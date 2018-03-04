f8496f912bd4cdaf2c9789c5206a42c4cfaddbf4
oreillyh <35709355+oreillyh@users.noreply.github.com>
2018-03-04 14:46:06 +0000

    credit to Mohamed for line.replace code

--- IrisData.py
+++ IrisData.py
@@ -2,8 +2,8 @@
 #Hugh O'Reilly 04/03/18
 with open("data/iris.csv") as f: #Opens Iris data set csv file in data folder
     for line in f:# loops through each line
-        line = line.replace(',', '  ') #replaces comma with space
-        line = line.rstrip() #Removes nextline code on end
+        line = line.replace(',', '  ') #replaces comma with space, code from Mohamed Noor
+        line = line.rstrip() #Removes nextline code on end,  code from Mohamed Noor
         print(line.split(',')[:5]) #Splits and Prints 
         #each line as a list, colon separates each item in
         # columns
