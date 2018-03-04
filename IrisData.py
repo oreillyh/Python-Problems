253c38c2f61cf851a09e20a15eba8c8066a9b7e4
oreillyh <35709355+oreillyh@users.noreply.github.com>
2018-03-04 13:54:13 +0000

    added text

--- IrisData.py
+++ IrisData.py
@@ -1,6 +1,10 @@
-with open("data/iris.csv") as f: 
-    for line in f:
-        print(line.split(',')[:])
+#Iris Data sorted into columns
+#Hugh O'Reilly 04/03/18
+with open("data/iris.csv") as f: #Opens Iris data set csv file in data folder
+    for line in f:# loops through each line
+        print(line.split(',')[:]) #Splits and Prints 
+        #each line as a list, colon separates each item in
+        # columns
     
             
 
