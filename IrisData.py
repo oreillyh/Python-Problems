f43b06dc3db89661ec65942c0bc384b31900b258
oreillyh <35709355+oreillyh@users.noreply.github.com>
2018-03-04 14:25:58 +0000

    another space

--- IrisData.py
+++ IrisData.py
@@ -2,7 +2,7 @@
 #Hugh O'Reilly 04/03/18
 with open("data/iris.csv") as f: #Opens Iris data set csv file in data folder
     for line in f:# loops through each line
-        line = line.replace(',', ' ') #replaces comma with space
+        line = line.replace(',', '  ') #replaces comma with space
         print(line.split(',')[:5]) #Splits and Prints 
         #each line as a list, colon separates each item in
         # columns
