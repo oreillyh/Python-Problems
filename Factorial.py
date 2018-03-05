b91105736addfc43b8d0cc14a099b9e26261df07
oreillyh <35709355+oreillyh@users.noreply.github.com>
2018-03-05 21:38:06 +0000

    slight correction

--- Factorial.py
+++ Factorial.py
@@ -1,7 +1,7 @@
 # Hugh O'Reilly 
     # 05/03/18
     # Defining a function to return the Factorial of an integer
-n = int(input('Enter a number: greater than 1:\n')) #prompts user to input integer
+n = int(input('Enter a number greater than 1:\n')) #prompts user to input integer
 def factorial(n):# defines the function n
     if n > 1: return n * factorial(n - 1) #calculates factorial
     elif n >= 0: return 1 #factorial is 1 if n is 0
