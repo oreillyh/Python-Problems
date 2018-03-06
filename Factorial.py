c5afbe140df5e1971fd439d416e1943ec0b86889
oreillyh <35709355+oreillyh@users.noreply.github.com>
2018-03-06 21:31:13 +0000

    removed elsif from factorial problem

--- Factorial.py
+++ Factorial.py
@@ -3,10 +3,9 @@
     # Defining a function to return the Factorial of an integer
 n = int(input('Enter a number greater than 1:\n')) #prompts user to input integer
 def factorial(n):# defines the function n
-    if n > 1: return n * factorial(n - 1) #calculates factorial
-    elif n >= 0: return 1 #factorial is 1 if n is 0
-    else: return ('n/a') #returns n/a for neg number input
-print ('The factorial of n is', factorial(n)) #prints output
+    if n == 0: return 1 #factorial is 1 if n is 0
+    else: return n * factorial(n - 1) #calculates factorial
+print ('The factorial of your number is', factorial(n)) #prints output
 
 
     
--- test2.py
+++ test2.py
@@ -1,4 +1,37 @@
-with open("data/iris.csv") as f:
-        for line in f:
-        print(line)
+# Hugh O'Reilly Euler Prob 5
+    # 05/03/18
+    # Solution using Factorial
+    # https://code.mikeyaworski.com/python/project_euler/problem_5
+    # 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+    # What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20? */
+
+    # returns smallest multiple that is evenly divisible by all numbers from 1 - n
+    # returns -1 if multiple does not exist
+    # works up to n = 20 (long reaches maximum value for values > 20) */
+def findSmallestMultiple(n):
+        i = n
+        while i < factorial(n):
+            if isMultiple(i, n):
+                return i
+            i += n
+        return -1
+
+    # checks every number between 1 and n to see if x is a multiple of every number
+    # returns True if x is found to be a multiple of every number, and False if x is
+    # found to not be a multiple of any number */ 
+def isMultiple(x, n):
+        for i in range(1, n):
+            if x % i != 0:
+                return False
+        return True
+
+    # returns the n factorial, or -1 if it does not exist
+def factorial(n):
+        if n > 1: return n * factorial(n - 1)
+        elif n >= 0: return 1
+        else: return -1
+
+print (findSmallestMultiple(10)) # 2520
+print (findSmallestMultiple(20))	
+
     
