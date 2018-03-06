9ab76574f8eef835168906c03da94186e349a360
oreillyh <35709355+oreillyh@users.noreply.github.com>
2018-03-06 21:32:23 +0000

    date change

--- Factorial.py
+++ Factorial.py
@@ -1,5 +1,5 @@
 # Hugh O'Reilly 
-    # 05/03/18
+    # 06/03/18
     # Defining a function to return the Factorial of an integer
 n = int(input('Enter a number greater than 1:\n')) #prompts user to input integer
 def factorial(n):# defines the function n
