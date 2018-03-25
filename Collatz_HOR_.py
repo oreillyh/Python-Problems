3e216a1c95a70cc10e5269d45d3674b7ab4b3cef
oreillyh <35709355+oreillyh@users.noreply.github.com>
2018-03-25 16:50:01 +0100

    Update Collatz_HOR_.py

--- Collatz_HOR_.py
+++ Collatz_HOR_.py
@@ -5,10 +5,10 @@ n = 17 # n assigned assigned the value of 17
 
 while n > 1: #while loop to define the conditions of n and create output based on odd or even iterations
     if  n % 2 == 0: #if statement creates function when n is even
-        n = (n//2)
+        n = (n//2) #
         print (n)
 
     else: # else statement creates function when n is not even
         n = (3*n + 1)
         print(n)
-
+# overall while loop returns n depending on odd or even conditions
