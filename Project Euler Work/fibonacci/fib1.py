85dc3fed2d469a1f3821b78fd8f0fa497ff11871
oreillyh <35709355+oreillyh@users.noreply.github.com>
2018-03-25 16:38:54 +0100

    Update fib1.py

--- Project Euler Work/fibonacci/fib1.py
+++ Project Euler Work/fibonacci/fib1.py
@@ -4,3 +4,5 @@ a, b = 0, 1
 while b < 100:
   print(b)
   a, b = b, a+b
+#Fibonacci sequence calculated by adding a and b and returning b back into a's position, 
+# iterated for each number in fib sequence
