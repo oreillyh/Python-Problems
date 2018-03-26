d48858c31dc9b77770b56420d0ac7d86a5b0d052
oreillyh <35709355+oreillyh@users.noreply.github.com>
2018-03-26 20:55:06 +0100

    Update and rename fib2.py to Fibonacci Exercise.py

--- Project Euler Work/fibonacci/Fibonacci Exercise.py
+++ Project Euler Work/fibonacci/Fibonacci Exercise.py
@@ -9,7 +9,14 @@ def fib(n):
     n = n - 1
   
   return i
-#lines 7 to 11 defines while loop when n is greater than 0, adding sum of i and j and returns i whcih is the nth fibobnacci number
+#lines 7 to 11 defines while loop when n is greater than 0, adding sum of i and j and returns i whcih is the nth fibonacci number
+
+#Text for Homework Week 2
+#My surname is O'Reilly
+#The first letter O is number 79
+#The last letter y is number 121
+#Fibonacci number 200 is 280571172992510140037611932413038677189525
+# ord() Returns an integer which is assigned to a unicode string character. Its inverse the chr() function returns a string character to an integer
 
 name = "O'Reilly"
 first = name[0]
